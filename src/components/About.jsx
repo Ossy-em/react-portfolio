eae5560fea16e4dcc7095994b84a6c3b8adea366
Ossy-em <emosinachi@gmail.com>
2024-11-15 18:36:23 +0100

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import aboutImg from "../assets/about.jpg"
+
+const About = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+        <h1 className="my-20 text-center text-4xl"> About 
+           <span className="text-neutral-500"> Me </span> 
+           </h1>
+
+    <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2 lg:p-8">
+        <div className="flex items-center justify-center">
+            <img className="rounded-2xl" src={aboutImg} alt="" />
+        </div>
+         
+        </div>
+
+    </div>
+
+    </div>
+  )
+ 
+}
+
+export default About
