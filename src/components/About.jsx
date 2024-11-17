7ff8a8dfec2dfc7d574bfe751a1820420b9a61fe
Ossy-em <emosinachi@gmail.com>
2024-11-17 17:25:16 +0100

    updated components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,24 @@
-import aboutImg from "../assets/about.jpg"
+import aboutImg from "../assets/about.jpg";
+import { ABOUT_TEXT } from '../constants';
 
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-        <h1 className="my-20 text-center text-4xl"> About 
-           <span className="text-neutral-500"> Me </span> 
-           </h1>
-
-    <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2 lg:p-8">
-        <div className="flex items-center justify-center">
-            <img className="rounded-2xl" src={aboutImg} alt="" />
+      <h1 className="my-20 text-center text-4xl">
+        About <span className="text-neutral-500">Me</span>
+      </h1>
+      <div className="flex flex-col lg:flex-row">
+        {/* Image Section */}
+        <div className="w-full lg:w-1/2 lg:p-8 flex items-center justify-center">
+          <img className="rounded-2xl max-w-full h-auto" src={aboutImg} alt="About" />
         </div>
-         
+        {/* Text Section */}
+        <div className="w-full lg:w-1/2 lg:p-8 flex items-center">
+          <p className="my-2 max-w-2xl py-6 text-center lg:text-left">{ABOUT_TEXT}</p>
         </div>
-
+      </div>
     </div>
+  );
+};
 
-    </div>
-  )
- 
-}
-
-export default About
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import {HERO_CONTENT}  from"../constants"
+import { motion } from "framer-motion"
 import profilePic from "../assets/kevinRushProfile.jpg"
 
 const Hero = () => {
@@ -7,7 +8,7 @@ const Hero = () => {
         <div className="w-full lg:w-1/2">
         <div className="flex flex-col items-center lg:items-start">
             <h1 className="pb-16 text-6xl font-thin trqcking-tight lg:mt-16 lg:text-8xl" >Ossy Emeruwa</h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"> 
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"> 
                 Frontend Developer
             </span>
             <p className="my-2 max-w-xlfont-light tracking-tighter">
