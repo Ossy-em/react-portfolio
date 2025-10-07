87b6c6fbb544e1dde16e55e03c808f2827d73a04
Ossy-em <emosinachi@gmail.com>
2025-10-07 23:49:27 +0100

    Added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+
+      <footer className="py-8 px-6 bg-gray-900 border-t border-gray-800 text-center text-gray-400">
+        <p>© 2025 Ossy Emeruwa. Built with React & Tailwind CSS.</p>
+      </footer>
+
+  )
+}
+
+export default Footer
+
